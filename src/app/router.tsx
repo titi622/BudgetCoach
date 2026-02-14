1f529bfe99fecb570f337529fed5bf5d9bc56daa
titi622 <ataejin622@gmail.com>
2026-02-15 08:23:12 +0900

    Update router.tsx

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import { LoginPage } from "../pages/LoginPage";
-import { HomePage } from "../pages/HomePage";
+import { LoginPage } from "../pages/LoginPage.tsx";
+import { HomePage } from "../pages/HomePage.tsx";
 
 export const router = createBrowserRouter([
   { path: "/", element: <LoginPage /> },
