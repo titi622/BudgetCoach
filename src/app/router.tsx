c0a93e5cd4d749360befd775e5cc615328eaa29f
titi622 <taejin@Taejinui-MacBookPro.local>
2026-03-25 15:32:31 +0900

    change router

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -3,10 +3,11 @@ import { createBrowserRouter } from "react-router-dom";
 import { LoginPage } from "../pages/LoginPage.tsx";
 import HomePage from "../pages/HomePage.tsx";
 import { AssetInputPage } from "../pages/AssetInputPage.tsx";
-
+import QueryTest from "../pages/QueryTest.tsx";
 
 export const router = createBrowserRouter([
   { path: "/", element: <LoginPage /> },
   { path: "/home", element: <HomePage /> },
   { path: "/add/:category", element: <AssetInputPage /> },
+  { path: "/test", element: <QueryTest /> },
 ]);
